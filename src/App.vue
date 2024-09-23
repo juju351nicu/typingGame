06d23b73d0acb6d2c9a7198eb18495e6d78e836d
k_shintani <ushio114mou@gmail.com>
2024-09-24 01:01:09 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,312 @@
+<template>
+  <div class="game-board">
+    <div class="words-board" ref="words_board">
+      <template v-for="word in currentWords">
+        <div class="word" :style="word.style">
+          <template v-for="(character, index) in word.characters">
+            <span :class="word.classList[index]">{{ character }} </span>
+          </template>
+        </div>
+      </template>
+    </div>
+    <template v-if="isGameStarted">
+      <input
+        type="text"
+        class="word-input"
+        v-model="inputValue"
+        @input="
+          checkWordEquality();
+          checkCharacter();
+        "
+      />
+      <div class="game-status">
+        <div class="game-status-item">
+          <label>Time</label>
+          <span>{{ getTime }}</span>
+        </div>
+        <div class="game-status-item">
+          <label>Score</label>
+          <span>{{ score }}</span>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <div class="game-options">
+        <div class="game-option">
+          <label>Game Mode </label>
+          <select @change="setGameMode" v-model="selectedGameMode">
+            <option
+              v-for="(gameMode, index) in config.modes"
+              :value="index"
+              :selected="index == selectedGameMode"
+            >
+              {{ gameMode }}
+            </option>
+          </select>
+        </div>
+        <div class="game-option">
+          <button class="btn" @click="play">Play<span class="btn-arrow">➔</span></button>
+        </div>
+      </div>
+    </template>
+  </div>
+  <Modal
+    :class="modalDisplayStatus ? 'open' : ''"
+    :isgameover="gameOver"
+    :modes="config.modes"
+    @restart-game="restartGame"
+  />
+</template>
+<script>
+import Modal from "./components/Modal.vue";
+import { words } from "./assets/words.js";
+export default {
+  name: "App",
+  data() {
+    return {
+      words: words,
+      currentWords: [],
+      gameOver: false,
+      isGameStarted: false,
+      wordIndex: 0,
+      inputValue: "",
+      selectedGameMode: 0,
+      score: 0,
+      modalDisplayStatus: false,
+      config: {
+        modes: ["Easy", "Normal", "Hard"],
+        wordStyleWidth: 200,
+        wordInsertionSpeeds: [4, 3, 2],
+        wordAnimationSpeeds: [60, 30, 15],
+        currentInsertionSpeed: 4,
+        currentAnimationSpeed: 60,
+      },
+      interval: {
+        insertion: null,
+        animation: null,
+        timer: null,
+      },
+      timer: {
+        second: 0,
+        minute: 0,
+        hour: 0,
+      },
+    };
+  },
+  components: {
+    Modal,
+  },
+  computed: {
+    /**
+     * 
+     */
+    getTime() {
+      let second = this.timer.second > 9 ? this.timer.second : `0${this.timer.second}`;
+      let minute = this.timer.minute > 9 ? this.timer.minute : `0${this.timer.minute}`;
+      let hour = this.timer.hour > 9 ? this.timer.hour : `0${this.timer.hour}`;
+      return `${hour}:${minute}:${second}`;
+    },
+  },
+  created() {
+    this.selectedGameMode =
+      Number(this.getLocalStorageData("gameMode")) || this.selectedGameMode;
+    this.setWordAnimationSpeed();
+  },
+  mounted() {
+    this.shuffleWords();
+  },
+  methods: {
+    /**
+     * ボタンをクリックするとゲームがスタートする
+     */
+    play() {
+      this.isGameStarted = true;
+      this.startTimer();
+      this.addWord();
+      this.interval.insertion = setInterval(() => {
+        this.addWord();
+      }, this.config.currentInsertionSpeed * 1000);
+      this.interval.animation = setInterval(() => {
+        this.wordsTopToBottom();
+        this.checkIsTopToBottom();
+      }, this.config.currentAnimationSpeed);
+    },
+    checkWordEquality() {
+      if (this.gameOver) return;
+      var word = this.inputValue;
+      let wordIndex = this.currentWords.findIndex(
+        (item) => item.characters.join("") == word
+      );
+      if (wordIndex != -1) {
+        this.removeWord(wordIndex);
+        this.inputValue = "";
+        this.increaseScore();
+        this.checkGameCompleted();
+      }
+    },
+    checkCharacter() {
+      if (this.gameOver) return;
+      const inputValue = this.inputValue.split("");
+      this.currentWords.forEach((word, wordIndex) => {
+        word.characters.forEach((character, characherIndex) => {
+          if (inputValue[characherIndex] == null) {
+            this.currentWords[wordIndex].classList[characherIndex] = "";
+          } else if (character == inputValue[characherIndex]) {
+            this.currentWords[wordIndex].classList[characherIndex] = "correct";
+          } else {
+            this.currentWords[wordIndex].classList[characherIndex] = "incorrect";
+          }
+        });
+      });
+    },
+    checkGameCompleted() {
+      if (this.isAddedAllWords() && this.currentWords.length == 0) {
+        this.gameFinish();
+      }
+    },
+    checkIsTopToBottom() {
+      let wordsBoardTop = this.$refs.words_board.offsetHeight;
+      this.currentWords.forEach((_, index) => {
+        let wordPositionTop = this.getCurrentWordTop(index);
+        if (wordPositionTop > wordsBoardTop) {
+          this.gameFinish();
+        }
+      });
+    },
+    gameFinish() {
+      this.gameOver = true;
+      this.clearInterval();
+      this.saveGameScores();
+      setTimeout(() => {
+        this.modalDisplayToggle();
+      }, 500);
+    },
+    saveGameScores() {
+      let gameScores = JSON.parse(this.getLocalStorageData("gameScores")) || [];
+      gameScores.push({
+        time: this.getTime,
+        score: this.score,
+        mode: this.selectedGameMode,
+      });
+      this.setLocalStorageData("gameScores", JSON.stringify(gameScores));
+    },
+    addWord() {
+      if (!this.isAddedAllWords()) {
+        this.currentWords.push({
+          characters: this.words[this.wordIndex].split(""),
+          classList: [],
+          style: {
+            left: `${this.getRandomPosition()}px`,
+            top: "-30px",
+          },
+        });
+        this.wordIndex++;
+      }
+    },
+    wordsTopToBottom() {
+      this.currentWords.forEach((_, index) => {
+        this.increasePositionTop(index);
+      });
+    },
+    shuffleWords() {
+      for (let wordIndex = this.words.length - 1; wordIndex > 0; wordIndex--) {
+        const randomIndex = Math.floor(Math.random() * wordIndex);
+        const tempWord = this.words[wordIndex];
+        this.words[wordIndex] = this.words[randomIndex];
+        this.words[randomIndex] = tempWord;
+      }
+    },
+    restartGame() {
+      this.resetGameData();
+      this.shuffleWords();
+      this.modalDisplayToggle();
+    },
+    getLocalStorageData(dataName) {
+      return localStorage.getItem(dataName);
+    },
+    getWordsBoardWidth() {
+      return this.$refs.words_board.offsetWidth;
+    },
+    getRandomPosition() {
+      return Math.floor(
+        Math.random() * (this.getWordsBoardWidth() - this.config.wordStyleWidth)
+      );
+    },
+    getCurrentWordTop(wordIndex) {
+      return Number(this.currentWords[wordIndex].style.top.slice(0, -2));
+    },
+    getWordsLength() {
+      this.words.length;
+    },
+    setLocalStorageData(dataName, data) {
+      localStorage.setItem(dataName, data);
+    },
+    setGameMode() {
+      this.setLocalStorageData("gameMode", this.selectedGameMode);
+      this.setWordAnimationSpeed();
+    },
+    setWordAnimationSpeed() {
+      this.config.currentAnimationSpeed = this.config.wordAnimationSpeeds[
+        this.selectedGameMode
+      ];
+      this.config.currentInsertionSpeed = this.config.wordInsertionSpeeds[
+        this.selectedGameMode
+      ];
+    },
+    removeWord(wordIndex) {
+      this.currentWords.splice(wordIndex, 1);
+    },
+    increasePositionTop(wordIndex) {
+      this.currentWords[wordIndex].style.top = `${
+        this.getCurrentWordTop(wordIndex) + 1
+      }px`;
+    },
+    increaseScore() {
+      this.score++;
+    },
+    modalDisplayToggle() {
+      this.modalDisplayStatus = !this.modalDisplayStatus;
+    },
+    isAddedAllWords() {
+      return this.words.length == this.wordIndex;
+    },
+    clearInterval() {
+      clearInterval(this.interval.animation);
+      clearInterval(this.interval.insertion);
+      clearInterval(this.interval.timer);
+    },
+    startTimer() {
+      this.interval.timer = setInterval(() => {
+        this.timer.second++;
+        if (this.timer.second === 60) {
+          this.timer.second = 0;
+          this.timer.minute++;
+        }
+        if (this.timer.minute === 60) {
+          this.timer.hour++;
+          this.timer.minute = 0;
+          this.timer.second = 0;
+        }
+        if (this.timer.hour === 24) {
+          this.timer.hour = 0;
+          this.timer.minute = 0;
+          this.timer.second = 0;
+        }
+      }, 1000);
+    },
+    resetGameData() {
+      this.currentWords = [];
+      this.timer = {
+        second: 0,
+        minute: 0,
+        hour: 0,
+      };
+      this.score = 0;
+      this.gameOver = false;
+      this.isGameStarted = false;
+      this.wordIndex = 0;
+      this.inputValue = "";
+    },
+  },
+};
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title">Game Completed</h1>
+      </div>
+      <div class="modal-body">
+        <p>{{ getLastScoreDesc }}</p>
+        <div class="game-option">
+          <select v-model="currentListMode">
+            <option disabled value="">List By Game Mode</option>
+            <option value="">All</option>
+            <option v-for="(mode, index) in modes" :value="index">{{ mode }}</option>
+          </select>
+        </div>
+        <template v-if="listGameScores.length > 0">
+          <h2>All Game Scores</h2>
+          <div class="game-scores-container">
+            <table class="game-scores">
+              <tr>
+                <th>No</th>
+                <th>Game Mode</th>
+                <th>Time</th>
+                <th>Score</th>
+              </tr>
+              <tr v-if="gameScores" v-for="(score, index) in listGameScores">
+                <td>{{ index + 1 }}</td>
+                <td>{{ getGameMode(score.mode) }}</td>
+                <td>{{ score.time }}</td>
+                <td>{{ score.score }}</td>
+              </tr>
+            </table>
+          </div>
+        </template>
+        <template v-else> There is no score information. </template>
+      </div>
+      <div class="modal-footer">
+        <button class="btn" @click="restartGame">
+          Play again <span class="btn-arrow">➔</span>
+        </button>
+        <button
+          class="btn"
+          @click="
+            resetGameScoreFromStorage();
+            resetModalData();
+          "
+        >
+          Reset Scores<span class="btn-arrow">➔</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Modal",
+  props: ["isgameover", "modes"],
+  data() {
+    return {
+      gameScores: [],
+      lastScore: {
+        score: "",
+        mode: "",
+        time: "",
+      },
+      currentListMode: "",
+    };
+  },
+  computed: {
+    getLastScoreDesc() {
+      if (this.gameScores.length > 0) {
+        let desc = `You completed ${this.lastScore.score} words in ${
+          this.lastScore.time
+        } time in ${this.getGameMode(this.lastScore.mode)} mode.`;
+        return desc;
+      }
+      return;
+    },
+    listGameScores() {
+      if (typeof this.currentListMode == "number") {
+        return this.gameScores.filter(
+          (gameScore) => gameScore.mode == this.currentListMode
+        );
+      }
+      return this.gameScores;
+    },
+  },
+  watch: {
+    isgameover: {
+      handler: function (status) {
+        if (status) {
+          this.getGameScores();
+          this.lastScore = this.gameScores[this.gameScores.length - 1];
+          this.sortGameScores();
+        } else {
+          this.resetModalData();
+        }
+      },
+    },
+  },
+  methods: {
+    restartGame() {
+      this.$emit("restart-game");
+    },
+    getGameScores() {
+      this.gameScores = JSON.parse(localStorage.getItem("gameScores")) || this.gameScores;
+    },
+    sortGameScores() {
+      this.gameScores.sort(
+        (a, b) => b.mode.toString().localeCompare(a.mode) || b.score - a.score
+      );
+    },
+    resetGameScoreFromStorage() {
+      localStorage.removeItem("gameScores");
+    },
+    getGameMode(index) {
+      return this.modes[Number(index)];
+    },
+    resetModalData() {
+      this.gameScores = [];
+      this.currentListMode = "";
+      this.lastScore = {
+        score: "",
+        mode: "",
+        time: "",
+      };
+    },
+  },
+};
+</script>
