d4be0b803d5c3912da1cd9c4a6e121d94acbf471
新谷健 <shintaniken@shintanikennoMacBook-puro.local>
2025-04-26 20:19:18 +0900

    htmlの修正

--- src/App.vue
+++ src/App.vue
@@ -319,3 +319,320 @@ export default {
   },
 };
 </script>
+<style>
+:root {
+    --main-bg-color: #DB2777;
+    --black: #000000;
+    --white: #ffffff;
+    --lime: #00FF00;
+    --red: #ff0000;
+    --gray: #888888;
+    --color-alto: #dddddd;
+    --color-emperor: #555555;
+    --color-malibu: #7bc1f7;
+    --color-gainsboro: #e0e0e0;
+}
+
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+a {
+    text-decoration: none;
+    color: var(--black);
+}
+
+html {
+    font-size: 10px;
+}
+
+body {
+    background-color: var(--main-bg-color);
+}
+
+.game-board {
+    width: 100vmin;
+    height: 90vmin;
+    display: flex;
+    flex-direction: column;
+    margin: 5vmin auto 0;
+    -webkit-box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.75);
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.75);
+}
+
+.words-board {
+    background-color: var(--color-gainsboro);
+    color: var(--black);
+    height: 75%;
+    padding: .5rem;
+    font-size: 2.4rem;
+    position: relative;
+    overflow: hidden;
+}
+
+.word {
+    position: absolute;
+    padding: 0 1rem;
+    background-color: rgba(0, 0, 0, 0.75);
+    color: var(--white);
+    border-radius: 1rem;
+}
+
+.word span {
+    font-size: 2rem;
+}
+
+.word-input {
+    width: 90%;
+    margin: 1.5rem auto 0;
+    height: 3.6rem;
+    font-size: 1.6rem;
+    padding: 1.5rem;
+    border: 1px solid var(--color-alto);
+    border-radius: 4px;
+    -moz-border-radius: 4px;
+    -webkit-border-radius: 4px;
+}
+
+.word-input:focus {
+    outline: none;
+    border: 1px solid var(--color-malibu);
+    box-shadow: 0px 0px 8px var(--color-malibu);
+    -moz-box-shadow: 0px 0px 8px var(--color-malibu);
+    -webkit-box-shadow: 0px 0px 8px var(--color-malibu);
+}
+
+.game-status {
+    display: flex;
+    justify-content: space-evenly;
+    color: var(--white);
+    margin-top: 1.5rem;
+}
+
+.game-status-item {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+}
+
+.game-board label {
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-bottom: .5rem;
+    color: var(--white);
+}
+
+.game-board span {
+    font-size: 2rem;
+}
+
+.game-options {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    grid-gap: 1.5rem;
+    padding: 1.5rem 0;
+}
+
+.game-option {
+    display: flex;
+    flex-direction: column;
+}
+
+.game-option select {
+    font-size: 1.6rem;
+    padding: 1rem;
+    border: none;
+    outline: none;
+    border-radius: 4px;
+    background-color: transparent;
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.5);
+    color: var(--white);
+}
+
+.game-option select option {
+    background-color: var(--main-bg-color);
+}
+
+.correct {
+    color: var(--lime);
+}
+
+.incorrect {
+    color: var(--red);
+}
+
+
+/* Modal */
+
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--black);
+    background-color: rgba(0, 0, 0, .5);
+    opacity: 0;
+    visibility: hidden;
+    transition: all .3s ease-out;
+    color: var(--white);
+}
+
+.modal.open {
+    visibility: visible;
+    opacity: 1
+}
+
+.modal.open .modal-content {
+    transform: translateY(0)
+}
+
+.modal-content {
+    width: 80%;
+    max-width: 100rem;
+    max-height: 55rem;
+    margin: 2.5rem auto 0;
+    border-radius: .6rem;
+    background-color: var(--main-bg-color);
+    border: .6rem solid var(--white);
+    box-shadow: 0 8px 24px rgba(0, 0, 0, .75);
+    transition: all .3s ease-out;
+    transform: translateY(-100rem);
+    overflow-y: auto;
+}
+
+.modal-header {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center
+}
+
+.modal-title {
+    font-size: 2rem;
+    font-weight: 500
+}
+
+.close {
+    cursor: pointer;
+    font-size: 3rem
+}
+
+.modal-body {
+    padding: 2rem 1rem;
+    border-top: 1px solid var(--color-alto);
+    border-bottom: 1px solid var(--color-alto);
+    font-size: 2rem;
+    text-align: center
+}
+
+.modal-body p {
+    padding: 0 5rem 2rem;
+}
+
+.modal-footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    grid-gap: 2.5rem
+}
+
+.btn {
+    outline: none;
+    border: none;
+    text-align: center;
+    font-size: 1.5rem;
+    border-radius: .3rem;
+    transition: all .5s;
+    background-color: transparent;
+    color: var(--white);
+    align-items: center;
+    cursor: pointer;
+    box-shadow: 0 1px 6px rgba(0, 0, 0, .75);
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    min-width: 12rem;
+}
+
+.btn:hover .btn-arrow {
+    padding-left: 1rem
+}
+
+.btn-arrow {
+    width: 2.5rem;
+    transition: all .3s ease-in-out
+}
+
+.game-scores-container {
+    width: 100%;
+    max-width: 60rem;
+    max-height: 24rem;
+    overflow-y: auto;
+    box-shadow: var(--bs-lg);
+    margin: 1rem auto 0;
+    box-shadow: 0 1px 6px rgba(0, 0, 0, .75);
+}
+
+.game-scores {
+    width: 100%;
+    border-collapse: collapse;
+    font-size: 1.8rem;
+}
+
+.game-scores td,
+.game-scores th {
+    text-align: left;
+    padding: 1rem;
+}
+
+.game-scores tr:not(:last-child) td,
+th {
+    border-bottom: 1px solid var(--color-alto);
+}
+
+.modal .game-option {
+    justify-content: flex-end;
+    align-items: center;
+    max-width: 20rem;
+    margin-left: auto;
+    font-size: 1.5rem;
+}
+
+.modal .game-option select {
+    min-width: 15rem;
+    font-size: 1.5rem;
+}
+
+::-webkit-scrollbar {
+    width: .8rem;
+}
+
+::-webkit-scrollbar-track {
+    background: var(--color-alto);
+}
+
+::-webkit-scrollbar-thumb {
+    background: var(--gray);
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: var(--color-emperor);
+}
+
+@media screen and (max-width: 600px) {
+    .game-board {
+        margin: 0;
+        -webkit-box-shadow: none;
+        -moz-box-shadow: none;
+        box-shadow: none;
+        height: 55rem;
+    }
+}
+</style>
