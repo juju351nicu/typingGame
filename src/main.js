658a9816cfda18bd2c9b847ff5258d6a1b67a78e
新谷健 <shintaniken@shintanikennoMacBook-puro.local>
2026-01-12 22:59:33 +0900

    fix package.json

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import { createPinia } from "pinia";
 import { createPersistedState } from "pinia-plugin-persistedstate";
-import VueSidebarMenu from "vue-sidebar-menu";
-import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import router from "@/router";
 import "@mdi/font/css/materialdesignicons.css";
 // Vuetify
@@ -12,9 +10,4 @@ import { createVuetify } from "vuetify";
 const pinia = createPinia();
 pinia.use(createPersistedState());
 const vuetify = createVuetify({});
-createApp(App)
-  .use(router)
-  .use(VueSidebarMenu)
-  .use(pinia)
-  .use(vuetify)
-  .mount("#app");
+createApp(App).use(router).use(pinia).use(vuetify).mount("#app");
