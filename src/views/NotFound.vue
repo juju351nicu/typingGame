777c9c6eacba51903c3f5a4dee1d45dde20d886b
新谷健 <shintaniken@shintanikennoMacBook-puro.local>
2026-01-28 15:26:47 +0900

    fix NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,19 @@
-<script setup lang="js">
+<script setup lang="ts">
 import { useRouter } from "vue-router";
 const router = useRouter();
 /**
  * トップページに戻る
  */
-const goDashBoard = () => {
+const goDashBoard = (): void => {
     router.push({ name: "DashBoard" });
 };
 </script>
 <template>
-    <h1>404: Not Found</h1>
-    <p>ページが見つかりません。</p>
-    <v-btn class="mt-2" color="success" @click="goDashBoard" size="large" width="150px">
-        &laquo; トップページに戻る
-    </v-btn>
+    <v-container>
+        <h1>404: Not Found</h1>
+        <p>ページが見つかりません。</p>
+        <v-btn class="mt-2" color="success" @click="goDashBoard" size="large">
+            トップページに戻る
+        </v-btn>
+    </v-container>
 </template>
